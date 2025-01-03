e60fb1a2eefaaf7120902671f392b176bb559afe
taot <1624024615@qq.com>
2025-01-03 12:52:17 +0000

    [feat](): add control param for struct and onnxruntime
        1 add control param for struct and onnxruntime

--- onnxdiff/main.py
+++ onnxdiff/main.py
@@ -6,14 +6,17 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--onnx_a", default="./", type=str, help="ONNX model a to compare")
     parser.add_argument("--onnx_b", default="./", type=str, help="ONNX model b to compare")
+    parser.add_argument("--struct", default=1, type=int, help="compare with structs and parameters")
+    parser.add_argument("--onnxruntime", default=1, type=int, help="compare with onnxruntime")
     args = parser.parse_args()
 
     assert(args.onnx_a[-5:] == ".onnx" and args.onnx_b[-5:] == ".onnx"), f"onnx_a and onnx_b are both expected path end with \'.onnx\'"
     
     model0 = onnx.load(args.onnx_a)
     model1 = onnx.load(args.onnx_b)
-    differ = OnnxDiff(model0, model1)
-    results = differ.summary(output=True)
+    if args.struct:
+        differ = OnnxDiff(model0, model1)
+        results = differ.summary(output=True)
 
 if __name__ == "__main__":
     main()
